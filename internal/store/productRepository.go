4375b08c18634c9666341bc97779dfe55e96f2b3
MirzaDgtu <mirzyali@mail.ru>
2024-10-08 23:43:56 +0700

    add product methods

--- internal/store/productRepository.go
+++ internal/store/productRepository.go
@@ -4,10 +4,8 @@ import "obstore/internal/model"
 
 type ProductRepository interface {
 	Create(model.Product) (model.Product, error)
-	DeleteByArticle(string) error
-	Update(model.Product) (model.Product, error)
 	GetByArticle(string) (model.Product, error)
-	GetByStrikeCode(string) (model.Product, error)
+	GetByStrikeCode(string) ([]model.Product, error)
 	GetByName(string) ([]model.Product, error)
 	GetAll() ([]model.Product, error)
 }
